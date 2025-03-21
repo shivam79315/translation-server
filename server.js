a4a88c89bdbffe1ec726f920a75c3523c10b7c84
Shivam <158444124+shivam79315@users.noreply.github.com>
2025-03-21 18:05:54 +0530

    Update server.js 11

--- server.js
+++ server.js
@@ -33,28 +33,28 @@ app.get('/', async (req, res) => {
     }
 });
 
-// app.post('/translate', async (req, res) => {
-//     const { text, from, to } = req.body;
-//     console.log("Request:", text);
-//     if (!text) {
-//         return res.status(400).json({ error: "Text is required" });
-//     }
+app.post('/translate', async (req, res) => {
+    const { text, from, to } = req.body;
+    console.log("Request:", text);
+    if (!text) {
+        return res.status(400).json({ error: "Text is required" });
+    }
 
-//     try {
-//         const result = await reverso.getTranslation(text, from, to);
-//         console.log("API Response:", result);
-//         // Ensure translation exists and is an array
-//         if (result) {
-//             res.json({ translation: result });
-//             console.log("Translation:", result);
-//         } else {
-//             res.status(500).json({ error: "No translation available." });
-//         }
-//     } catch (error) {
-//         console.error("Error:", error);
-//         res.status(500).json({ error: "Failed to fetch translation." });
-//     }
-// });
+    try {
+        const result = await reverso.getTranslation(text, from, to);
+        console.log("API Response:", result);
+        // Ensure translation exists and is an array
+        if (result) {
+            res.json({ translation: result });
+            console.log("Translation:", result);
+        } else {
+            res.status(500).json({ error: "No translation available." });
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ error: "Failed to fetch translation." });
+    }
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
